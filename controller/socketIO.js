2a725d4a91fd79a84ab6a1489131cab34d0f23e8
Kim-144 <g12leader144@gmail.com>
2020-10-14 17:39:31 +0900

    [soketIO,facebook]server:construction:

--- controller/socketIO.js
+++ controller/socketIO.js
@@ -7,6 +7,8 @@ module.exports = (io) => {
         .then(result => io.emit('list-live-stream', result))
     }
 
+     /* Get list live stream information */
+
     io.on('connection', (socket) => {
       console.log('New connection');
 
@@ -15,6 +17,40 @@ module.exports = (io) => {
           .findAll()
           .then(result => socket.emit('list-live-stream', result))
       })
-    })
-    
+
+      /* Join live stream room */
+
+      socket.on('join-room', (data) => {
+        console.log('Join room', data);
+        const { userName, roomName } = data;
+        if (!userName || !roomName) return;
+        socket.join(roomName);
+      });
+
+      socket.on('leave-room', (data) => {
+        console.log('Leave room', data);
+        const { userName, roomName } = data;
+        if (!userName || !roomName) return;
+        socket.leave(roomName);
+      });
+
+      socket.on('prepare-live-stream', (data) => {
+        console.log('Prepare live stream', data);
+        const { userName, title, body } = data;
+
+        broadcast
+              .findOrCreate({
+                where: {
+                  userId :session_userid 
+                },
+                defaults: {
+                  title: title,
+                  body: body,
+                  status: 'PR'
+                }
+                .then(createdData => emitListLiveStreamInfo())
+      
+      })
+      })
+})
 }
